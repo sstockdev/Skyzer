15b8a8d33e8f3463c08ed4a3e5f8db99c6ea39ab
Sam Stockstrom <144393153+sstockdev@users.noreply.github.com>
2026-08-26 11:25:59 -0500

    add ended auctions api endpoint to constants

--- Constants.cs
+++ Constants.cs
@@ -11,5 +11,10 @@
         /// The starting page of the auctions API endpoint.
         /// </summary>
         public const int ACTIVE_AUCTIONS_STARTING_PAGE = 0;
+
+        /// <summary>
+        /// The ended auctions API endpoint from Hypixel API.
+        /// </summary>
+        public const string ENDED_AUCTIONS_URL = "https://api.hypixel.net/v2/skyblock/auctions_ended";
     }
 }
